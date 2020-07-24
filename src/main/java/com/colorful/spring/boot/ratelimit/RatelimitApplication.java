b47e69e3711a61348b727869d1c2ac55e41c20e8
hongby <hongby@szinfinova.com>
2020-07-24 15:08:35 +0800

    feat : 初始化项目

---
+++ src/main/java/com/colorful/spring/boot/ratelimit/RatelimitApplication.java
@@ -0,0 +1,13 @@
+package com.colorful.spring.boot.ratelimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatelimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatelimitApplication.class, args);
+    }
+
+}
